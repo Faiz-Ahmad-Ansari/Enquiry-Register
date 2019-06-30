7adf7321514583eb04c9145c51f0fc4eb4803f95
Your Name <ansarifaiz32@gmail.com>
2019-07-01 00:08:23 +0530

    1.3 added form

--- hello.js
+++ hello.js
@@ -14,16 +14,24 @@ function newEnquiry() {
     cell1.innerHTML=enqNum;
 
     var enqDt = document.getElementById("Enquiry Date").value;
-    console.log(enqDt);
-    cell2.innerHTML=enqDt;
+    var enqDate = enqDt.substr(8,2);
+    var enqMonth = enqDt.substr(5,2);
+    var enqYear = enqDt.substr(0,4);
+    var enqDateInput = enqDate + "/" + enqMonth + "/" + enqYear ;
+
+    cell2.innerHTML=enqDateInput;
     
     var clientName = document.getElementById("Client Name").value;
     cell3.innerHTML=clientName;
 
-    var dueDate = document.getElementById("Due Date").value;
+    var dueDt = document.getElementById("Due Date").value;
+    var dDate = dueDt.substr(8,2);
+    var dMonth = dueDt.substr(5,2);
+    var dYear = dueDt.substr(0,4);
+    var dueDate = dDate + "/" + dMonth + "/" + dYear ;
     cell4.innerHTML=dueDate;
 
-}
+//}
 
 // For highlight row as per todays date
     var fullDate = new Date();
@@ -47,4 +55,34 @@ function newEnquiry() {
                     tr[i].style.backgroundColor="yellow";    
                 }
         }
-    }        
+    }
+    document.getElementById("myForm").style.display="none";
+}
+
+    // For highlight row as per todays date
+    var fullDate = new Date();
+
+    var date = fullDate.getDate();
+    var month = fullDate.toISOString().substr(5,2);
+    var year = fullDate.getFullYear();
+
+    var myDate = date + "/" + month + "/" + year ;
+    
+    var table = document.getElementById("myTable");
+    var tr = table.getElementsByTagName("tr");
+ 
+    for (i=0; i < tr.length; i++){
+        td = tr[i].getElementsByTagName("td")[3];
+            if (td) {
+                txtValue = td.innerText;
+             //   console.log(td);
+        
+                if (txtValue == myDate) {
+                    tr[i].style.backgroundColor="yellow";    
+                }
+        }
+    }
+//open form button
+function openForm() {
+    document.getElementById("myForm").style.display="block";
+}
